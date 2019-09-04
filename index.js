0938bfeb6069d3f721d07c7d51bea86b0978eca9
nanbal <nanbalkumdam@gmail.com>
2019-09-04 12:45:46 +0100

    My Completed Assignment

--- index.js
+++ index.js
@@ -237,16 +237,16 @@ var check = students.filter(function(record){
 console.log(check);
 
 //CALCULATE THE AVERAGE GRADES AND OUPUT THE MAXIMUM
-var arr = 0;
+var maxavg = 0;
 var ans ;
 
 students.forEach((value,index,data) =>{
     var sum = value.grade.reduce((previous,current) => current+previous);
     var avg = sum/value.grade.length;
-   if(arr < avg){
-       arr = avg;
+   if(maxavg < avg){
+       maxavg = avg;
        ans = index;
    }
 });
-console.log("\n\nThis Student has the maximum grade \n\n" ,students[ans]);
+console.log("\n\nThis Student has the maximum grade Average \n\n" ,students[ans]);
 
